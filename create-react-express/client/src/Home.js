f76324ef5604dec0f0f50936b6f7124d5d9466cc
Yadira Soto <lunar13heart@hotmail.com>
2019-08-06 00:35:24 -0400

    edited home & profile layouts

--- create-react-express/client/src/Home.js
+++ create-react-express/client/src/Home.js
@@ -1,8 +1,70 @@
 import React from "react"
 
 export const Home = () => (
-    <div>
-    <h1>Hello</h1>
-    <p>This is the home page</p>
-    </div>
+
+    <div className="App">
+        <div className="App-header">
+        <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2>Welcome to the Anime Emporium</h2>
+            </div>
+
+            <div class="col">
+                genre drop down
+            </div>
+
+            <div class="col">
+                search bar 
+            </div>
+
+              <div class="col">
+                login/sign up 
+            </div>
+
+</div>
+</div>
+</div>
+
+        <p className="App-intro">
+          <div class="container">
+            <div class="row">
+              <div class="col">
+                upcoming anime:
+                </div>
+
+              <div class="col">
+                random anime
+                </div>
+
+            </div>
+            <br></br>
+
+
+            <div class="row">
+              <div class="col">
+              carosal
+                </div>
+            </div>
+
+            <div class="row">
+              <div class="col">
+              popular anime
+                </div>
+            </div>
+
+            <div class="row">
+              <div class="col">
+              all time favorite anime
+                </div>
+            </div>
+          </div>
+
+
+<br></br>
+        We are An-Y.
+        </p>
+      <p>We are a duo team, which consists of Andrew & Yadira, based out of Orlando, FL.</p>
+      </div >
+    
 )
--- create-react-express/client/src/Profile.js
+++ create-react-express/client/src/Profile.js
@@ -1,8 +1,40 @@
 import React from "react"
 
 export const Profile = () => (
-    <div>
-    <h1>Hello</h1>
-    <p>This is your profile page</p>
-    </div>
+
+    <div className="App">
+        
+        <div class="container">
+        <div class="row">
+            <div class="col">
+                <h2>Profile</h2>
+                <br></br>
+                <p>image</p>
+                <p>namee</p>
+                <p>joined</p>
+                <p>forum-comment top 3</p>
+            </div>
+
+</div>
+</div>
+
+
+<br></br>
+
+<div class="container">
+<div class="row">
+<div class="col">
+                completed
+            </div>
+
+<div class="col">
+                watch list
+            </div>
+
+</div>
+
+</div>
+
+      </div >
+    
 )
