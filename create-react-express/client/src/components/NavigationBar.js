14477b74bc327bbb5c98e8baa348647e7c07337e
Yadira Soto <lunar13heart@hotmail.com>
2019-08-08 19:44:33 -0400

    more awesome edits

--- create-react-express/client/src/components/NavigationBar.js
+++ create-react-express/client/src/components/NavigationBar.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Modal from "./Modal";
+import Modal2 from "./Modal2";
 import {
   Collapse,
   Navbar,
@@ -7,12 +9,9 @@ import {
   Nav,
   NavItem,
   NavLink,
-  Dropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
   Button, Form, FormGroup, Label, Input
 } from "reactstrap";
+import Dropdown from "./Dropdown";
 
 export default class Example extends React.Component {
   constructor(props) {
@@ -36,72 +35,8 @@ export default class Example extends React.Component {
           <NavbarToggler onClick={this.toggle} />
           <Collapse isOpen={this.state.isOpen} navbar>
             <Nav className="ml-auto" navbar>
-
-              <Dropdown isOpen={this.state.isOpen} toggle={this.toggle}>
-                <DropdownToggle>
-                  Dropdown
-        </DropdownToggle>
-                <DropdownMenu
-                  modifiers={{
-                    setMaxHeight: {
-                      enabled: true,
-                      order: 890,
-                      fn: (data) => {
-                        return {
-                          ...data,
-                          styles: {
-                            ...data.styles,
-                            overflow: 'auto',
-                            maxHeight: 100,
-                          },
-                        };
-                      },
-                    },
-                  }}
-                >
-                  <DropdownItem>Action</DropdownItem>
-                  <DropdownItem>Adventure</DropdownItem>
-                  <DropdownItem>Cars</DropdownItem>
-                  <DropdownItem>Comedy</DropdownItem>
-                  <DropdownItem>Dementia</DropdownItem>
-                  <DropdownItem>Demons</DropdownItem>
-                  <DropdownItem>Mystery</DropdownItem>
-                  <DropdownItem>Drama</DropdownItem>
-                  <DropdownItem>Fantasy</DropdownItem>
-                  <DropdownItem>Game</DropdownItem>
-                  <DropdownItem>Historical</DropdownItem>
-                  <DropdownItem>Horror</DropdownItem>
-                  <DropdownItem>Kids</DropdownItem>
-                  <DropdownItem>Magic</DropdownItem>
-                  <DropdownItem>Martial Arts</DropdownItem>
-                  <DropdownItem>Mecha</DropdownItem>
-                  <DropdownItem>Music</DropdownItem>
-                  <DropdownItem>Parody</DropdownItem>
-                  <DropdownItem>Samurai</DropdownItem>
-                  <DropdownItem>Romance</DropdownItem>
-                  <DropdownItem>School</DropdownItem>
-                  <DropdownItem>Sci Fi</DropdownItem>
-                  <DropdownItem>Shoujo</DropdownItem>
-                  <DropdownItem>Shounen</DropdownItem>
-                  <DropdownItem>Space</DropdownItem>
-                  <DropdownItem>Sports</DropdownItem>
-                  <DropdownItem>Super Power</DropdownItem>
-                  <DropdownItem>Vampire</DropdownItem>
-                  <DropdownItem>Yaoi</DropdownItem>
-                  <DropdownItem>Yuri</DropdownItem>
-                  <DropdownItem>Harem</DropdownItem>
-                  <DropdownItem>Slice of Life</DropdownItem>
-                  <DropdownItem>Supernatural</DropdownItem>
-                  <DropdownItem>Military</DropdownItem>
-                  <DropdownItem>Police</DropdownItem>
-                  <DropdownItem>Psychological</DropdownItem>
-                  <DropdownItem>Thriller</DropdownItem>
-                  <DropdownItem>Seinen</DropdownItem>
-                  <DropdownItem>Josei</DropdownItem>
-                </DropdownMenu>
-              </Dropdown>
-
-<br></br>
+            <Dropdown/>
+              <br></br>
 
               <NavItem>
                 <NavLink href="#">
@@ -117,7 +52,11 @@ export default class Example extends React.Component {
                 </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="#">Login/Sign Up</NavLink>
+                <NavLink href="#"><Modal/></NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="#"><Modal2/></NavLink>
               </NavItem>
             </Nav>
           </Collapse>
